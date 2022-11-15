461bf9641bc69bba859d6c58c837619a1041d2eb
JesusRamirez37 <>
2022-11-15 15:07:07 +0000

    Remik and Jesus

---
+++ .ccls-cache/@home@runner@Lab-13-pt-2/211-f22-collab-github@collab.c
@@ -0,0 +1,37 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void rwisni2_jrami42(){
+  printf("\nRemik Wisniewski and Jesus Ramirez\n");
+  printf("Initially we had trouble figuring out the correct syntax for the Git commands. After figuring how to format the commands, it was quite simple to make it work.\n");
+  printf("We learned how to write the commands to pull projects into the environment and make changes to the files.");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    rwisni2_jrami42();
+    return 0;
+}
